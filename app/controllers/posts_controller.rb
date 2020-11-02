7e92c09d7512fd429d910915f2ea841492cedfed
EC2 Default User <ec2-user@ip-172-31-46-185.ec2.internal>
2020-11-02 14:44:27 +0000

    4終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,37 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+     @posts = Post.all.order(created_at: :desc)
   end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    # フォームから送信されたデータを受け取り、保存する処理を追加してください
+    @post = Post.new(content:params[:content])
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    @post = redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+ get 'posts/index'
+ get "posts/new" => "posts#new"
+ get "posts/:id" => "posts#show"
+ post "posts/create" => "posts#create"
+ get "posts/:id/edit" => "posts#edit"
+ post "posts/:id/update" => "posts#update"
+ post "posts/:id/destroy" => "posts#destroy"
 
  get "/" => "home#top"
  get "about" => "home#about"
