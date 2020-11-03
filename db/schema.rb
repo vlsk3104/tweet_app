e5f108201bc127c0f50732b034206b07981304d1
EC2 Default User <ec2-user@ip-172-31-46-185.ec2.internal>
2020-11-03 12:07:34 +0000

    7.finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201103031332) do
+ActiveRecord::Schema.define(version: 20201103111817) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20201103031332) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_name"
   end
 
 end
