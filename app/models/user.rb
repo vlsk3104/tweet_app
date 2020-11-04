9c5df36814b46f81e4c5b88b4b0b409270f4b47a
EC2 Default User <ec2-user@ip-172-31-46-185.ec2.internal>
2020-11-04 04:20:41 +0000

    8.ログイン機能終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  
+
    validates :name, {presence: true}
-   
+
    validates :email, {presence: true, uniqueness: true}
-   
+
+   validates :password, {presence: true}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+ get "login" => "users#login_form"
+ post "login" => "users#login"
+ post "logout" => "users#logout"
+
  post "users/:id/update" => "users#update"
  get "users/:id/edit" => "users#edit"
  post "users/create" => "users#create"
- get "signup" => "users#new" 
+ get "signup" => "users#new"
  get "users/index" => "users#index"
  get "users/:id" => "users#show"
- 
+
  get "posts/index" => "posts#index"
  get "posts/new" => "posts#new"
  get "posts/:id" => "posts#show"
