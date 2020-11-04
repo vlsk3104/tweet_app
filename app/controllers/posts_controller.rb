e2616cfb9363b90528d266ea6f7934edc6e94ba0
EC2 Default User <ec2-user@ip-172-31-46-185.ec2.internal>
2020-11-04 05:29:17 +0000

    9.投稿とユーザーの紐付け終了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
+   before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
   def index
      @posts = Post.all.order(created_at: :desc)
   end
 
   def show
     @post = Post.find_by(id: params[:id])
+    @user = @post.user
   end
 
   def new
@@ -13,7 +15,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(content:params[:content])
+    @post = Post.new(
+      content: params[:content],
+
+      user_id: @current_user.id
+    )
+
     if @post.save
        flash[:notice] = "投稿を作成しました"
     redirect_to("/posts/index")
@@ -43,6 +50,14 @@ class PostsController < ApplicationController
     @post.destroy
     flash[:notice] = "投稿を削除しました"
     redirect_to("/posts/index")
+  end
+
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
 
    validates :password, {presence: true}
 
+  def posts
+    return Post.where(user_id: self.id)
+  end
+
 end
