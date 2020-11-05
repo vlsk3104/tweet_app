29d154058083f332bab72017c6ace2f30ed830fa
EC2 Default User <ec2-user@ip-172-31-46-185.ec2.internal>
2020-11-05 00:54:31 +0000

    11終了

--- Gemfile
+++ Gemfile
@@ -21,6 +21,9 @@ gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+
+gem 'bcrypt'
+
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201104053207) do
+ActiveRecord::Schema.define(version: 20201104235135) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(version: 20201104053207) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "image_name"
-    t.string   "password"
+    t.string   "password_digest"
   end
 
 end
